e2b91ab487f5009b80d586f1d3b58b4926062efc
mxw1081 <157513536+mxw1081@users.noreply.github.com>
2024-02-20 11:05:49 -0500

    Add files via upload
    Handson_Upload file

---
+++ Handons/Handson 3/conversions.py
@@ -0,0 +1,7 @@
+def fFeetToMeter():
+    x = int(input("\nEnter feet:"))
+    return print("\n",round(x*0.3048,2),"Meter/s")
+
+def fMeterToFeet():
+    x = int(input("\nEnter meter:"))
+    return print("\n",round(x/0.3048,2),"Feet")
